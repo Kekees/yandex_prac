a57240b2edbfe87aab67a4dacdd6cc00a41447e0
Dmitry Nemtsov <dn@MacBook-Air-Dmitry.local>
2022-12-14 00:27:35 +0300

    Реализовал Greeting + рефакторинг

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 """Генератор приветствий."""
-import pprint
+
 
 def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
+    """Возвращает текст приветствия.
 
       Args:
           name: Имя пользователя
@@ -10,5 +10,6 @@ def Greeting(name: str) -> str:
       Returns:
           int: Текст приветствия
       """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    # pprint.pprint(name.lower())
+
+    return 'Привет, ' + name.title()
